66068982038aa829bbd51fda4db74ff0f094f5e3
Your Name <you@example.com>
2024-03-28 00:17:50 +0600

    fix path name

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./ui/navbar";
+import Navbar from "./ui/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
